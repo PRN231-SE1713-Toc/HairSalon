d0d1b4e275c7b70d2dc4c64b66e877a65da4a320
Vu Long <vulongbinhduong@gmail.com>
2024-09-18 11:02:01 +0700

    [Add] Jwt configuration in appsettings and TokenService

--- PRN231.SE1713.Toc/HairSalon.Service/TokenService.cs
+++ PRN231.SE1713.Toc/HairSalon.Service/TokenService.cs
@@ -1,7 +1,10 @@
 ﻿using HairSalon.Core.Contracts.Services;
 using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Security.Cryptography;
+using System.Text;
 
 namespace HairSalon.Service
 {
@@ -11,20 +14,19 @@ namespace HairSalon.Service
 
         public string GenerateAccessToken(IEnumerable<Claim> claims)
         {
-            //var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(JwtConfigurations.Key));
-            //var signInCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+            var jwtSettings = _configuration.GetSection("JwtSettings");
+            var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings["Key"] ?? string.Empty));
+            var signInCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
 
-            //var jwtOptions = new JwtSecurityToken(
-            //    issuer: JwtConfigurations.Issuer,
-            //    audience: JwtConfigurations.Audience,
-            //    claims: claims,
-            //    expires: DateTime.Now.AddMinutes(10),
-            //    signingCredentials: signInCredentials
-            //);
-            //var token = new JwtSecurityTokenHandler().WriteToken(jwtOptions) ?? string.Empty;
-            //return token;
-
-            return string.Empty;
+            var jwtOptions = new JwtSecurityToken(
+                issuer: jwtSettings["Issuer"],
+                audience: jwtSettings["Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(15),
+                signingCredentials: signInCredentials
+            );
+            var token = new JwtSecurityTokenHandler().WriteToken(jwtOptions) ?? string.Empty;
+            return token;
         }
 
         public string GenerateRefreshToken()
@@ -40,7 +42,25 @@ namespace HairSalon.Service
 
         public ClaimsPrincipal GetPrincipalFromAccessToken(string token)
         {
-            throw new NotImplementedException();
+            var jwtSettings = _configuration.GetSection("JwtSettings");
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidIssuer = jwtSettings["Issuer"],
+                ValidAudience = jwtSettings["Audience"],
+                ValidateAudience = true,
+                ValidateIssuer = true,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings["Key"] ?? string.Empty)),
+                ValidateLifetime = false 
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            SecurityToken securityToken;
+            var principal = tokenHandler.ValidateToken(token, tokenValidationParameters, out securityToken);
+            var jwtSecurityToken = securityToken as JwtSecurityToken;
+            if (jwtSecurityToken == null || !jwtSecurityToken.Header.Alg.Equals(SecurityAlgorithms.HmacSha256, StringComparison.InvariantCultureIgnoreCase))
+                throw new SecurityTokenException("Invalid token");
+            return principal;
         }
     }
 }
